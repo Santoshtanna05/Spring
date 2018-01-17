a3d7fcf1216f20ffba868c0f5c47d7846129e273
Santosh <Santosh.tanna05@gmail.com>
2018-01-17 10:36:44 -0800

    java_code

---
+++ src/com/jlcindia/spring/Lab1.java
@@ -0,0 +1,15 @@
+package com.jlcindia.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Lab1 {
+	public static void main(String[] args) {
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("jlcindia.xml");
+		System.out.println("spring container is ready");
+		System.out.println("---------");
+		Hello h=(Hello) ctx.getBean("hello");
+		h.show();
+	}
+
+}
